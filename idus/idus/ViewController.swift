5b25bc14653cdd4009fc9ef5da2328aeefb978e3
RooZnn <dnwlsskfn@gmail.com>
2022-06-25 21:17:14 +0900

    init

---
+++ idus/idus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idus
+//
+//  Created by RooZin on 2022/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
